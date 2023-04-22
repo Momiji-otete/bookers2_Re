db6c4125158155bb4f7e47ead2974039cac3f01a
Momiji-otete <126852371+Momiji-otete@users.noreply.github.com>
2023-04-22 13:57:28 +0000

    [Update]make bookindex leftside

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   has_one_attached :profile_image
   has_many :books, dependent: :destroy
+
+  def get_profile_image
+    (profile_image.attached?) ? profile_image : "no_image.jpg"
+  end
 end
