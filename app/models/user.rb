7bdf7bbcf5c58462bac62b36b205c538b61a609a
Momiji-otete <126852371+Momiji-otete@users.noreply.github.com>
2023-04-29 06:28:08 +0000

    [Update]閲覧数追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,8 @@ class User < ApplicationRecord
   has_many :followings, through: :relationships, source: :followed
   has_many :followers, through: :reverse_relationships, source: :follower
 
+  has_many :view_counts, dependent: :destroy
+
   validates :name, uniqueness: true, length: { minimum: 2, maximum: 20 }
   validates :introduction, length: { maximum: 50 }
 
